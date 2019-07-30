e77c7b38c748558c4f2bb95bbc795503f0a729bf
Prashant <prashantsengar5@gmail.com>
2019-07-30 15:09:54 +0530

    More platform independent

--- battery.py
+++ battery.py
@@ -1,20 +1,27 @@
 import psutil
 import time
-#notify the thing
+import sys
+platform_is_windows = sys.platform.startswith('win')
+if platform_is_windows:
+	from win10toast import ToastNotifier
+else:
+	import subprocess as sc
 
-from win10toast import ToastNotifier
-
-def notify(title, msg):
-    noti = ToastNotifier()
-    noti.show_toast(title, msg, duration=10)
+if platform_is_windows:
+	def notify(title, msg):
+		noti = ToastNotifier()
+		noti.show_toast(title, msg, duration=10)
+else:
+	def notify(title, msg):
+		sc.call(['notify-send', title, msg])
 
 while True:
 	batt = psutil.sensors_battery()
 
-	if batt.percent <= 40:
+	if batt.percent <= 40 and batt.power_plugged is False:
 		notify('Battery Low', 'Please plug-in a charger Sir')
 	elif batt.percent >=80 and batt.power_plugged is True	:
 		notify('Battery charged', 'Please remove the charger Sir')
 	
-	for i in range(600):
+	for __ in range(600):
 		time.sleep(1)
